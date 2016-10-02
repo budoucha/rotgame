adb9f1ef5dd6e98b65e2b92a422c6c5a5e5dd4c6
Haruto Kanno <budoucha@gmail.com>
2016-10-02 19:52:41 +0900

    当たり判定、スコアなど

--- sketch.js
+++ sketch.js
@@ -15,6 +15,12 @@ function setup() {
   rectH = tsize / 2;
   pixelDensity(1);
   bulletExists = false;
+  bulletSize = long / 32;
+
+  rectWidth = long / 4;
+  rectHeight = long / 26;
+  reflecNoise = long / 255;
+  score = 0;
 }
 
 function draw() {
@@ -23,18 +29,19 @@ function draw() {
   update();
   drawBar();
   drawBullet();
+  drawScore();
 }
 
 function update() {
   updateRot();
   updateBullet();
-
-
+  check();
+  score++;
 }
 
 
 function updateRot() {
-  rotZ = rotationZ;
+  rotZ = radians(-rotationZ);
   rotX = rotationX;
   rotY = rotationY;
 }
@@ -44,22 +51,32 @@ function updateBullet() {
     mkBullet();
   }
 
-  if (abs(bulletX + bulletVX - width / 2) >= width / 2) {
-    bulletVX = -bulletVX;
+  if (abs(bulletX + bulletVX) + bulletSize >= width / 2) {
+    bulletVX = -bulletVX + random(-reflecNoise, reflecNoise);
+    bulletVY += random(-reflecNoise, reflecNoise);
   }
-  if (abs(bulletY + bulletVY - height / 2) >= height / 2) {
-    bulletVY = -bulletVY;
+  if (abs(bulletY + bulletVY) + bulletSize >= height / 2) {
+    bulletVX += random(-reflecNoise, reflecNoise);
+    bulletVY = -bulletVY + random(-reflecNoise, reflecNoise);
   }
   bulletX += bulletVX;
   bulletY += bulletVY;
 }
 
+function check() {
+  if (abs(bulletX) - bulletSize < rectWidth && abs(bulletY) - bulletSize < rectHeight) {
+    mkBullet();
+  }
+}
 
 function mkBullet() {
-  bulletX = width / 2;
-  bulletY = height;
-  bulletVY = random(-long / 160, long / 160);
-  bulletVX = random(-long / 160, long / 160);
+  bulletX = 0;
+  bulletY = height / 2 - bulletSize;
+  v0 = long / 160;
+  theta0 = random(0, -PI);
+  bulletVY = v0 * sin(theta0);
+  bulletVX = v0 * cos(theta0);
+  score = 0;
   bulletExists = true;
 }
 
@@ -71,24 +88,37 @@ function rmBullet() {
   bulletExists = false;
 }
 
-function mouseClicked(){
+function mouseClicked() {
   mkBullet();
 }
 
 function drawBar() {
   push();
   translate(width / 2, height / 2);
-  rotate(radians(-rotZ));
+  rotate(rotZ);
   fill(255);
   rectMode(RADIUS);
-  rect(0, 0, long / 4, long / 26);
+  rect(0, 0, rectWidth, rectHeight);
   pop();
 }
 
 function drawBullet() {
+  push();
+  translate(width / 2, height / 2);
   ellipseMode(RADIUS);
   fill(255, 0, 0);
-  ellipse(bulletX, bulletY, long / 32, long / 32);
+  ellipse(bulletX, bulletY, bulletSize, bulletSize);
+  pop();
+}
+
+function drawScore() {
+  push();
+  translate(width / 2, height / 2);
+  fill(0, 216, 255);
+  textAlign(CENTER);
+  textSize(short / 12);
+  text("SCORE:\n"+score, 0, -vgrid*3);
+  pop();
 }
 
 function windowResized() {
