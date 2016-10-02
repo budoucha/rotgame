76b8a3b0beeb8c250d0650252779d055b5004e73
U-BDCSURFACE\Haruto <budoucha@gmail.com>
2016-10-02 16:39:37 +0900

    Detchupped

--- sketch.js
+++ sketch.js
@@ -2,6 +2,8 @@ function setup() {
   createCanvas(windowWidth, windowHeight, P2D);
 
   frameRate(60);
+  short = min(width, height);
+  long = max(width, height);
   vgrid = height / 8;
   hgrid = width / 8;
   du = min(vgrid, hgrid);
@@ -12,25 +14,81 @@ function setup() {
   rateZ = width / 360;
   rectH = tsize / 2;
   pixelDensity(1);
+  bulletExists = false;
 }
 
 function draw() {
-  update();
+
   background(0);
-  translate(width / 2, height / 2);
-  rotate(-rotZ);
-  fill(255);
-  rectMode(RADIUS);
-  rect(0,0,hgrid*2,vgrid/4);
-
-  fill(255);
-
+  update();
+  drawBar();
+  drawBullet();
 }
 
 function update() {
+  updateRot();
+  updateBullet();
+
+
+}
+
+
+function updateRot() {
   rotZ = rotationZ;
   rotX = rotationX;
   rotY = rotationY;
+}
+
+function updateBullet() {
+  if (bulletExists === false) {
+    mkBullet();
+  }
+
+  if (abs(bulletX + bulletVX - width / 2) >= width / 2) {
+    bulletVX = -bulletVX;
+  }
+  if (abs(bulletY + bulletVY - height / 2) >= height / 2) {
+    bulletVY = -bulletVY;
+  }
+  bulletX += bulletVX;
+  bulletY += bulletVY;
+}
+
+
+function mkBullet() {
+  bulletX = width / 2;
+  bulletY = height;
+  bulletVY = random(-long / 160, long / 160);
+  bulletVX = random(-long / 160, long / 160);
+  bulletExists = true;
+}
+
+function rmBullet() {
+  bulletX = -100;
+  bulletY - 100;
+  bulletVX = 0;
+  bulletVY = 0;
+  bulletExists = false;
+}
+
+function mouseClicked(){
+  mkBullet();
+}
+
+function drawBar() {
+  push();
+  translate(width / 2, height / 2);
+  rotate(radians(-rotZ));
+  fill(255);
+  rectMode(RADIUS);
+  rect(0, 0, long / 4, long / 26);
+  pop();
+}
+
+function drawBullet() {
+  ellipseMode(RADIUS);
+  fill(255, 0, 0);
+  ellipse(bulletX, bulletY, long / 32, long / 32);
 }
 
 function windowResized() {
